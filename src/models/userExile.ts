7a9c781d0df23d88d1ac13cb9dd2d53014356df3
Brandon <brandong9@sky.com>
2021-01-11 16:34:08 +0000

    Updating Schema and types

--- src/models/userExile.ts
+++ src/models/userExile.ts
@@ -2,6 +2,8 @@ import { model, Schema } from 'mongoose';
 import { ExileTypes } from '../types/userExile';
 
 const ExileSchema: Schema = new Schema({
+  Moderator: { type: String },
+  Reason: { type: String },
   RobloxUsername: { type: String },
   RobloxID: { type: Number },
 });
--- src/types/userExile.d.ts
+++ src/types/userExile.d.ts
@@ -1,6 +1,8 @@
 import { Document } from 'mongoose';
 
 export interface ExileTypes extends Document {
+  Moderator: String;
+  Reason: String;
   RobloxUsername: String;
   RobloxID: Number;
 }
