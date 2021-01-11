698bcfbb5af6ecbf7b318bbcf0306ab1ec749d43
Brandon <brandong9@sky.com>
2021-01-11 16:34:26 +0000

    Removing command doesn't exist message

--- src/events/commandHandler/message.ts
+++ src/events/commandHandler/message.ts
@@ -8,8 +8,4 @@ export = async (bot: any, message: any) => {
   if (!message.content.startsWith(prefix)) return;
   const commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
   if (commandfile) commandfile.run(bot, message, args);
-
-  if (message.content.startsWith(prefix) && !commandfile) {
-    message.channel.send('Command does not exist, use .help for a full list of commands.');
-  }
 };
