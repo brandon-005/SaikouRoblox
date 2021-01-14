661e69e70413d826b0735cb7b923cc065950ef8a
Brandon <brandong9@sky.com>
2021-01-14 11:01:07 +0000

    loggin

--- src/events/commandHandler/message.ts
+++ src/events/commandHandler/message.ts
@@ -2,6 +2,8 @@ export = async (bot: any, message: any) => {
   const prefix = process.env.PREFIX;
   if (message.author.bot || message.channel.type === 'dm' || !message.content.startsWith(prefix)) return;
 
+  console.log('made it');
+
   const args = message.content.slice(prefix!.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
   const commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
