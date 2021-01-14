81ad3b4f3288879738bef6eb20acad27e8c6b3cb
Brandon <brandong9@sky.com>
2021-01-14 11:08:38 +0000

    logs

--- src/events/commandHandler/message.ts
+++ src/events/commandHandler/message.ts
@@ -1,5 +1,5 @@
 export = async (bot: any, message: any) => {
-  console.log('made it');
+  console.log('made it1');
   const prefix = process.env.PREFIX;
   if (message.author.bot || message.channel.type === 'dm' || !message.content.startsWith(prefix)) return;
 
@@ -7,7 +7,15 @@ export = async (bot: any, message: any) => {
   const cmd = args.shift().toLowerCase();
   const commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
 
+  console.log('made it2');
+
+  console.log(`cmd: ${cmd}`);
+
+  console.log(commandfile);
+
   if (commandfile === undefined) return;
   console.log(commandfile);
+  console.log('made it3');
   commandfile.run(bot, message, args);
+  console.log('should of executed');
 };
