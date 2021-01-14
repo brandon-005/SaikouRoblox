7264ba5af0a34b23e27c7b8842501427fc8dc48f
Brandon <brandong9@sky.com>
2021-01-14 11:24:46 +0000

    Update message.ts

--- src/events/commandHandler/message.ts
+++ src/events/commandHandler/message.ts
@@ -1,21 +1,18 @@
 export = async (bot: any, message: any) => {
-  console.log('made it1');
-  const prefix = process.env.PREFIX;
- // if (message.author.bot || message.channel.type === 'dm' || !message.content.startsWith(prefix)) return;
+  console.log('made it 1');
+  if (message.author.bot || message.channel.type === 'dm') return;
 
-  const args = message.content.slice(prefix!.length).trim().split(/ +/g);
+  console.log('made it 2');
+  const prefix = process.env.PREFIX!;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
-  const commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
-
-  console.log('made it2');
 
   console.log(`cmd: ${cmd}`);
 
+  if (!message.content.startsWith(prefix)) return;
+  console.log('made it 3');
+  const commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
   console.log(commandfile);
-
-  if (commandfile === undefined) return;
-  console.log(commandfile);
-  console.log('made it3');
-  commandfile.run(bot, message, args);
-  console.log('should of executed');
+  if (commandfile) commandfile.run(bot, message, args);
+  console.log('ran command');
 };
