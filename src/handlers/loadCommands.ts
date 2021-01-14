9248ed40c9c6268937fbd554fa6cc3964067422b
Brandon <brandong9@sky.com>
2021-01-14 10:16:42 +0000

    log2

--- src/handlers/loadCommands.ts
+++ src/handlers/loadCommands.ts
@@ -2,10 +2,10 @@ import { readdirSync } from 'fs';
 
 export = (bot: any) => {
   const load = (directories: string) => {
-    console.log('here')
     const commands = readdirSync(`${__dirname}/../commands/${directories}/`).filter((directoryFile) => directoryFile.endsWith('.js'));
     commands.forEach((commandFile) => {
       const command = require(`${__dirname}/../commands/${directories}/${commandFile}`);
+      console.log(command);
       bot.commands.set(command.config.name, command);
       if (command.config.aliases) command.config.aliases.forEach((alias: any) => bot.aliases.set(alias, command.config.name));
     });
