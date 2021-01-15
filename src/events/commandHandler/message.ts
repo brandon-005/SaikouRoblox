c37d18e1a586735d151ab2e4bf3a7b648d0bfc2d
Brandon <brandong9@sky.com>
2021-01-15 09:02:19 +0000

    Fixing the command handler (omg finally)

--- src/events/commandHandler/message.ts
+++ src/events/commandHandler/message.ts
@@ -1,16 +1,11 @@
 export = async (bot: any, message: any) => {
-  const prefix = process.env.PREFIX!;
-  console.log('made it 1');
-  console.log(prefix);
+  const prefix = '.';
   if (!message.content.startsWith(prefix) || message.author.bot || message.channel.type === 'dm') return;
 
-  console.log('made it 2');
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
-  console.log(`cmd: ${cmd}`);
 
   if (!message.content.startsWith(prefix)) return;
-  console.log('made it 3');
   const commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
   console.log(commandfile);
 
@@ -22,5 +17,4 @@ export = async (bot: any, message: any) => {
     console.error(err);
     message.reply('There was an error running the command.');
   }
-  console.log('ran command');
 };
