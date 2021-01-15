bd2c9674751303670139d4e3c7873a3a0ac52405
Brandon <brandong9@sky.com>
2021-01-15 16:05:26 +0000

    Minor status change

--- src/events/bot/ready.ts
+++ src/events/bot/ready.ts
@@ -3,7 +3,7 @@ import mongoDatabase from '../database/connectDatabase';
 export = async (bot: any) => {
   console.log(`\n${bot.user.username} has loaded successfully and is online.`);
 
-  bot.user.setActivity('hi', {
+  bot.user.setActivity('Bloxxing Players', {
     type: 'STREAMING',
     url: 'https://twitch.tv/doingthisforthestatuslol',
   });
