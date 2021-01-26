aff277f90d1c10f10bd2356612f81fa288af2826
Brandon <brandong9@sky.com>
2021-01-26 15:08:21 +0000

    Auto promote additions/changes

--- src/events/discordServer/guildMemberUpdate.ts
+++ src/events/discordServer/guildMemberUpdate.ts
@@ -1,50 +1,47 @@
+import { MessageEmbed } from 'discord.js';
 import rbx from 'noblox.js';
 
-export = async (_bot: any, oldMember: any, newMember: any) => {
-  console.log(oldMember.roles.cache.size);
-  console.log('Nickname:', newMember.nickname);
-  console.log('UserID:', newMember.user.id);
-
-  const oldRolesSize = oldMember.roles.cache.size;
-  const newRolesSize = newMember.roles.cache.size;
+export = async (bot: any, oldMember: any, newMember: any) => {
   const allUserRoles: string[] = [];
   let discordRole;
+  const RobloxName = newMember.nickname;
   let RobloxID;
 
   if (!newMember.nickname) return;
 
-  if (oldRolesSize !== newRolesSize) {
+  if (oldMember.roles.cache.size !== newMember.roles.cache.size) {
     newMember.roles.cache.forEach((role: any) => {
+      allUserRoles.push(role.name);
       if (!oldMember.roles.cache.has(role.id)) {
         discordRole = role.name;
       }
     });
   }
 
-  if (oldRolesSize !== newRolesSize) {
-    newMember.roles.cache.forEach((role: any) => {
-      allUserRoles.push(role.name);
-    });
-  }
-
   if (!discordRole) return;
 
-  console.log(`Name: ${newMember.nickname}\nRole: ${discordRole}`);
-
   try {
-    RobloxID = await rbx.getIdFromUsername(`${newMember.nickname}`);
+    RobloxID = await rbx.getIdFromUsername(RobloxName);
   } catch (err) {
     return;
   }
 
-  console.log(RobloxID);
-
   if (allUserRoles.includes('Staff')) return;
+
+  const logEmbed = new MessageEmbed() //
+    .setTitle(`:warning: Automatic Rankup!`)
+    .setColor('#FFD62F')
+    .setDescription(`**${RobloxName} was ranked up automatically by SaikouGroup**`)
+    .setFooter(`Ranked Player ID: ${RobloxID}`)
+    .setTimestamp();
 
   if (discordRole === 'Dedicated Follower') {
     try {
       await rbx.setRank(Number(process.env.GROUP), RobloxID, 'Dedicated Follower').then(() => {
-        console.log(`Ranked up ${newMember.nickname} to "Dedicated Follower."`);
+        logEmbed.addField('Ranked To:', 'Dedicated Follower');
+        logEmbed.addField('Rankup Reason:', 'User hit the "Dedicated Follower" role in Discord.');
+
+        bot.channels.cache.get(process.env.ADMIN_LOG).send(logEmbed);
       });
     } catch (err) {
       return;
@@ -54,7 +51,10 @@ export = async (_bot: any, oldMember: any, newMember: any) => {
   if (discordRole === 'Ultimate Follower') {
     try {
       await rbx.setRank(Number(process.env.GROUP), RobloxID, 'Ultimate Follower').then(() => {
-        console.log(`Ranked up ${newMember.nickname} to "Ultimate Follower."`);
+        logEmbed.addField('Ranked To:', 'Ultimate Follower');
+        logEmbed.addField('Rankup Reason:', 'User hit the "Ultimate Follower" role in Discord.');
+
+        bot.channels.cache.get(process.env.ADMIN_LOG).send(logEmbed);
       });
     } catch (err) {
       return;
@@ -64,7 +64,10 @@ export = async (_bot: any, oldMember: any, newMember: any) => {
   if (discordRole === 'Supreme Follower') {
     try {
       await rbx.setRank(Number(process.env.GROUP), RobloxID, 'Supreme Follower').then(() => {
-        console.log(`Ranked up ${newMember.nickname} to "Supreme Follower."`);
+        logEmbed.addField('Ranked To:', 'Supreme Follower');
+        logEmbed.addField('Rankup Reason:', 'User hit the "Supreme Follower" role in Discord.');
+
+        bot.channels.cache.get(process.env.ADMIN_LOG).send(logEmbed);
       });
     } catch (err) {
       return;
@@ -74,7 +77,10 @@ export = async (_bot: any, oldMember: any, newMember: any) => {
   if (discordRole === 'Legendary Follower') {
     try {
       await rbx.setRank(Number(process.env.GROUP), RobloxID, 'Legendary Follower').then(() => {
-        console.log(`Ranked up ${newMember.nickname} to "Legendary Follower."`);
+        logEmbed.addField('Ranked To:', 'Legendary Follower');
+        logEmbed.addField('Rankup Reason:', 'User hit the "Legendary Follower" role in Discord.');
+
+        bot.channels.cache.get(process.env.ADMIN_LOG).send(logEmbed);
       });
     } catch (err) {
       return;
@@ -84,7 +90,10 @@ export = async (_bot: any, oldMember: any, newMember: any) => {
   if (discordRole === 'Omega Follower') {
     try {
       await rbx.setRank(Number(process.env.GROUP), RobloxID, 'Omega Follower').then(() => {
-        console.log(`Ranked up ${newMember.nickname} to "Omega Follower."`);
+        logEmbed.addField('Ranked To:', 'Omega Follower');
+        logEmbed.addField('Rankup Reason:', 'User hit the "Omega Follower" role in Discord.');
+
+        bot.channels.cache.get(process.env.ADMIN_LOG).send(logEmbed);
       });
     } catch (err) {
       return;
