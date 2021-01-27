5a09fd91ac16f996f6d813f9db52275e81b621e6
Brandon <brandong9@sky.com>
2021-01-27 16:20:07 +0000

    Minor changes to guildMemberUpdate

--- src/events/discordServer/guildMemberUpdate.ts
+++ src/events/discordServer/guildMemberUpdate.ts
@@ -40,63 +40,47 @@ export = async (bot: any, oldMember: any, newMember: any) => {
       await rbx.setRank(Number(process.env.GROUP), RobloxID, 'Dedicated Follower').then(() => {
         logEmbed.addField('Ranked To:', 'Dedicated Follower');
         logEmbed.addField('Rankup Reason:', 'User hit the "Dedicated Follower" role in Discord.');
-
-        bot.channels.cache.get(process.env.ADMIN_LOG).send(logEmbed);
       });
     } catch (err) {
       return;
     }
-  }
-
-  if (discordRole === 'Ultimate Follower') {
+  } else if (discordRole === 'Ultimate Follower') {
     try {
       await rbx.setRank(Number(process.env.GROUP), RobloxID, 'Ultimate Follower').then(() => {
         logEmbed.addField('Ranked To:', 'Ultimate Follower');
         logEmbed.addField('Rankup Reason:', 'User hit the "Ultimate Follower" role in Discord.');
-
-        bot.channels.cache.get(process.env.ADMIN_LOG).send(logEmbed);
       });
     } catch (err) {
       return;
     }
-  }
-
-  if (discordRole === 'Supreme Follower') {
+  } else if (discordRole === 'Supreme Follower') {
     try {
       await rbx.setRank(Number(process.env.GROUP), RobloxID, 'Supreme Follower').then(() => {
         logEmbed.addField('Ranked To:', 'Supreme Follower');
         logEmbed.addField('Rankup Reason:', 'User hit the "Supreme Follower" role in Discord.');
-
-        bot.channels.cache.get(process.env.ADMIN_LOG).send(logEmbed);
       });
     } catch (err) {
       return;
     }
-  }
-
-  if (discordRole === 'Legendary Follower') {
+  } else if (discordRole === 'Legendary Follower') {
     try {
       await rbx.setRank(Number(process.env.GROUP), RobloxID, 'Legendary Follower').then(() => {
         logEmbed.addField('Ranked To:', 'Legendary Follower');
         logEmbed.addField('Rankup Reason:', 'User hit the "Legendary Follower" role in Discord.');
-
-        bot.channels.cache.get(process.env.ADMIN_LOG).send(logEmbed);
       });
     } catch (err) {
       return;
     }
-  }
-
-  if (discordRole === 'Omega Follower') {
+  } else if (discordRole === 'Omega Follower') {
     try {
       await rbx.setRank(Number(process.env.GROUP), RobloxID, 'Omega Follower').then(() => {
         logEmbed.addField('Ranked To:', 'Omega Follower');
         logEmbed.addField('Rankup Reason:', 'User hit the "Omega Follower" role in Discord.');
-
-        bot.channels.cache.get(process.env.ADMIN_LOG).send(logEmbed);
       });
     } catch (err) {
       return;
     }
-  }
+  } else return;
+
+  bot.channels.cache.get(process.env.ADMIN_LOG).send(logEmbed);
 };
