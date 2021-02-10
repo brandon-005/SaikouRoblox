506f74f6d2bf5d279e92208c54567378d2d6da4e
Brandon <brandong9@sky.com>
2021-02-10 16:46:28 +0000

    Update robloxHelp.ts

--- src/commands/general/robloxHelp.ts
+++ src/commands/general/robloxHelp.ts
@@ -27,7 +27,7 @@ export = {
           `The prefix for SaikouRoblox is \`.\`
 			Throwback to SaikouBot v1, entering the nostalgia zone`
         )
-        .addField('General - 3', '`shout`, `rankup`, `listwords`')
+        .addField('General - 2', '`shout`, `listwords`')
         .addField('Moderation - 3', '`suspend`, `permexile` `unexile`, `clearshout`')
         .addField('Management - 2', '`blacklist`, `removeBlacklist`')
         .setFooter("Keep up your hard work, it's much appreciated!")
